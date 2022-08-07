33621f38c6d28af159fc1d7628250d6f0073888b
Leticia F. Magrin <108889811+LeticiaMagrin@users.noreply.github.com>
2022-08-06 22:03:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -52,3 +52,16 @@ form.addEventListener('submit', (event) => {
     renderPokemon(input.value.toLowerCase());
 
 });
+buttonPrev.addEventListener('click', () => {
+    if (searchPokemon >1 ) {
+    searchPokemon -= 1;
+    renderPokemon(searchPokemon);
+ }
+});
+
+buttonNext.addEventListener('click', () => {
+    searchPokemon += 1;
+    renderPokemon(searchPokemon);
+
+});
+renderPokemon(searchPokemon);
